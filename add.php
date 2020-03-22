112231e3067031c01e9e7f21af7d8a374f053041
Ishan-002 <ishan26rawat@gmail.com>
2020-03-23 03:42:41 +0530

    Added feature to add book

--- add.php
+++ add.php
@@ -2,3 +2,33 @@
     <head>
         <title>Add New books</title>
     </head>
+    <body>
+        <form action="add.php" method="POST">
+            <label>Enter the name of the book:</label>
+            <input type="text" name="newbook">
+            <input type="submit" value="ADD">
+        </form>
+        <?php
+            $dbuser="ishu";
+            $dbpass="Ishan_002";
+            $dbhost="localhost";
+            $db="books";
+        
+            $conn=new mysqli($dbhost,$dbuser,$dbpass,$db);
+        
+            if($conn->connect_errno) {
+                die("connnection unsuccesful");
+            }
+        
+            else {
+                if(isset($_POST["newbook"])) {
+                $new=$_POST["newbook"];
+                $sql=$conn->query("INSERT INTO `booklist` (`ID`, `book name`, `availability`) VALUES (NULL, '$new', 'Available')");
+                if($sql) {
+                    echo("done!!");
+                }
+                }
+            }
+        ?>
+    </body>
+</html>
